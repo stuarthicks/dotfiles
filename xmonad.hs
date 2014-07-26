e090dcc40367b6f03ca45682edef9cac3a807c99
Stuart Hicks <contact@stuarthicks.me>
2014-07-26 12:26:58 +0100

    update xmonad config for keybindings

--- xmonad.hs
+++ xmonad.hs
@@ -3,10 +3,70 @@ import XMonad
 import XMonad.Layout.Spacing
 import XMonad.Hooks.DynamicLog
 
+import Data.Monoid
+import System.Exit
+ 
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+
+    -- app launch hotkeys
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modm,               xK_n     ), spawn "nautilus --no-desktop")
+    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+    , ((modm .|. shiftMask, xK_l     ), spawn "gnome-screensaver-command -l")
+    , ((modm .|. shiftMask, xK_s     ), spawn "gnome-screensaver-command -l && sudo pm-suspend")
+ 
+    -- close focused window
+    , ((modm .|. shiftMask, xK_c     ), kill)
+ 
+     -- Rotate through the available layout algorithms
+    , ((modm,               xK_space ), sendMessage NextLayout)
+ 
+    --  Reset the layouts on the current workspace to default
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+ 
+    -- Move focus
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_m     ), windows W.focusMaster  )
+ 
+    -- Swap the focused window with the master/next/prev window
+    , ((modm,               xK_Return), windows W.swapMaster)
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+ 
+    -- Shrink/expand the master area
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_l     ), sendMessage Expand)
+ 
+    -- Push window back into tiling
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+ 
+    -- Increment/Decrement the number of windows in the master area
+    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
+    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
+ 
+    -- Quit/Reload Xmonad
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+    ]
+    ++
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+
 main = xmonad =<< xmobar defaultConfig
   {
     terminal = "gnome-terminal",
     modMask = mod4Mask,
     borderWidth = 1,
-    layoutHook = smartSpacing 5 $ Tall 1 (3/100) (1/2)
+    layoutHook = smartSpacing 5 $ Tall 1 (3/100) (1/2),
+    keys = myKeys
   }
