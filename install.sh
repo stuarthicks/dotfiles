6d6cc177277e9a6977e994a0b8e1f9d90703f13e
Stuart Hicks <stuart.hicks@bbc.co.uk>
2014-09-18 18:09:11 +0900

    basic install script for first-use

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -u
+
+link () {
+  FILE=$1
+  ln -s etc/$FILE .$FILE
+}
+
+cd
+
+link zshrc
+link zprofile
+link vimrc
+link ackrc
+link aliases
+link mailcap
+link rubocop.yml
+link tmux.conf
+link pentadactylrc
+link pentadactyl
+link mintty_profile
+
+touch .path
+touch .profile
+
+# For ZSH plugin loading
+curl -sL https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > antigen.zsh
+
+# For VIM plugin loading
+mkdir -p ~/.vim/autoload
+curl -fsLo ~/.vim/autoload/plug.vim \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +"PlugInstall" +qall
+
