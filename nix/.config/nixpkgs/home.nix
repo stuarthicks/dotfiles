ffc1f03eb3ee82f5985c2876f642f93df5164285
Stuart Hicks <stuarthicks@users.noreply.github.com>
2020-11-05 10:34:26 +0000

    tidy config

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -1,6 +1,13 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: {
+  home.stateVersion = "20.09";
+  programs.home-manager.enable = true;
 
-{
+  home.username = "stuart";
+  home.homeDirectory = "/home/stuart";
+
+  home.language.base = "en_GB.UTF-8";
+  home.sessionVariables.LOCALES_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+
   home.packages = with pkgs; [
     bzip2
     cargo
@@ -35,25 +42,4 @@
     tmux
     zlib
   ];
-
-  home.language.base = "en_US.UTF-8";
-  home.sessionVariables.LOCALES_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "stuart";
-  home.homeDirectory = "/home/stuart";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "20.09";
 }
