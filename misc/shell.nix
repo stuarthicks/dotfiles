03298e5764e5cafbcd5c6cd703e28a0ac377bd7d
Stuart Hicks <stuarthicks@users.noreply.github.com>
2021-08-11 11:45:39 +0100

    update blank nix-shell

--- misc/shell.nix
+++ misc/shell.nix
@@ -1,9 +1,7 @@
-# https://nixos.wiki/wiki/Development_environment_with_nix-shell
-# https://github.com/direnv/direnv/wiki/Nix#setting-up-a-project-to-use-nix
-with import <nixpkgs> {};
-mkShell {
-  nativeBuildInputs = [
-    # dependencies you want available in your shell
-    # eg, zlib
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    hello
   ];
 }
