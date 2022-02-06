ca9962be1d12034b4c13b9bd9fab14b5a5336351
Stuart Hicks <stuarthicks@users.noreply.github.com>
2022-02-06 08:59:09 +0000

    auto run packer compile when editing plugins

--- neovim/.config/nvim/lua/stuarthicks/plugins.lua
+++ neovim/.config/nvim/lua/stuarthicks/plugins.lua
@@ -6,6 +6,13 @@ if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
