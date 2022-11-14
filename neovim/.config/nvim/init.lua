0710b2e7b005db3e7bc76009681a5f2eeb1ae6a2
Stuart Hicks <stuarthicks@users.noreply.github.com>
2022-11-14 10:12:27 +0000

    detect m3u8 files in vim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -93,3 +93,10 @@ vim.keymap.set('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_
 vim.keymap.set('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>')
 vim.keymap.set('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>')
+
+vim.cmd([[
+  augroup hls
+    autocmd!
+    autocmd BufRead *.m3u8 setfiletype hlsplaylist
+  augroup end
+]])
