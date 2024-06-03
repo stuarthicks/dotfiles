58667e293ca778695575b6f796031706edce4c9d
Stuart Hicks <stuarthicks@users.noreply.github.com>
2024-06-03 13:14:56 +0100

    add firenvim

--- neovim/.config/nvim/lua/stuarthicks/plugins.lua
+++ neovim/.config/nvim/lua/stuarthicks/plugins.lua
@@ -136,6 +136,16 @@ require("lazy").setup({
     ft = {"go", 'gomod'},
     build = ':lua require("go.install").update_all_sync()'
   },
+  {
+    'glacambre/firenvim',
+
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    lazy = not vim.g.started_by_firenvim,
+    build = function()
+      vim.fn["firenvim#install"](0)
+    end
+  },
   -- Themes
   {
     'folke/tokyonight.nvim',
