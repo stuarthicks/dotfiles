e9564b2e72af59210a99202341ec2c2c877bb10e
Stuart Hicks <stuarthicks@users.noreply.github.com>
2024-07-30 11:23:52 +0100

    update firenvim install

--- neovim/.config/nvim/lua/stuarthicks/plugins.lua
+++ neovim/.config/nvim/lua/stuarthicks/plugins.lua
@@ -166,9 +166,7 @@ require("lazy").setup({
     -- Lazy load firenvim
     -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
     lazy = not vim.g.started_by_firenvim,
-    build = function()
-      vim.fn["firenvim#install"](0)
-    end
+    build = ":call firenvim#install(0)",
   },
 
   {'akinsho/git-conflict.nvim', version = "*", config = true},
