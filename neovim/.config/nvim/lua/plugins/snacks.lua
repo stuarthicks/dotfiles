a4ee42f52deaf1b8d9af891d74436fd8b4eeafe2
Stuart Hicks <stuarthicks@users.noreply.github.com>
2025-01-26 11:14:17 +0000

    fix trouble sidebar

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -128,6 +128,8 @@ return {
 
   {
     "folke/trouble.nvim",
+    opts = {},
+    cmd = 'Trouble',
     keys = {
       { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",                        desc = "Diagnostics (Trouble)",                        },
       { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",           desc = "Buffer Diagnostics (Trouble)",                 },
