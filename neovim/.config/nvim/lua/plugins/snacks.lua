9f9ea66d1ae9bb99c77382fe2fc57fe270660dcf
Stuart Hicks <stuarthicks@users.noreply.github.com>
2025-04-01 11:20:57 +0100

    disable dashboard

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -6,7 +6,6 @@ return {
     ---@type snacks.Config
     opts = {
       bigfile      = { enabled = true  },
-      dashboard    = { enabled = true  },
       indent       = { enabled = true  },
       input        = { enabled = true  },
       picker       = { enabled = true  },
