42154b1afbbc0d72283f93196c2862ff2edd0b21
Stuart Hicks <stuarthicks@users.noreply.github.com>
2025-07-02 11:45:09 +0100

    disable max line length warning for markdown files

---
+++ neovim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,12 @@
+local HOME = os.getenv("HOME")
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
+      },
+    },
+  },
+}
