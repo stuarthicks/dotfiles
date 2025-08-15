e5c5b571fc73d68179a040a4d57996aed4d4a93d
Stuart Hicks <stuarthicks@users.noreply.github.com>
2025-08-15 21:21:09 +0100

    format conform settings

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -149,11 +149,26 @@ local conform = require("conform")
 
 conform.setup({
   formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "isort", "black" },
-    rust = { "rustfmt", lsp_format = "fallback" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    go = { "goimports", lsp_format = "fallback" },
+    lua = {
+      "stylua"
+    },
+    python = {
+      "isort",
+      "black"
+    },
+    rust = {
+      "rustfmt",
+      lsp_format = "fallback"
+    },
+    javascript = {
+      "prettierd",
+      "prettier",
+      stop_after_first = true
+    },
+    go = {
+      "goimports",
+      lsp_format = "fallback"
+    },
   },
 })
 
