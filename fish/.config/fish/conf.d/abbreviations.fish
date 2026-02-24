e1512a76745601d5e5261176dd813857de3d2523
Stuart Hicks <stuarthicks@users.noreply.github.com>
2026-02-24 15:09:28 +0000

    organise abbr

--- fish/.config/fish/conf.d/abbreviations.fish
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -1,45 +1,54 @@
-abbr -a -- as awsume
-abbr -a -- bathelp 'bat --plain --language=help'
-abbr -a -- cat 'bat --decorations=never'
-abbr -a -- cert 'tls_cert_info -address'
-abbr -a -- count_uniq 'sort | uniq -c | sort -nr'
-abbr -a -- cyberchef 'open "$HOME/Developer/src/github.com/gchq/CyberChef/build/prod/index.html"'
-abbr -a -- gcloud-clear-project "gcloud config unset project"
-abbr -a -- gron fastgron
+# ls / lsr
+abbr -a -- l 'lsr --group-directories-first --long'
+abbr -a -- la 'lsr --group-directories-first --long --almost-all'
+abbr -a -- ls lsr
+
+# EDITOR/VISUAL
+abbr -a -- e zed
+abbr -a -- h hx
+abbr -a -- hh "hx --health"
+abbr -a -- n nvim
+
+# tools
+abbr -a -- m mise
+abbr -a -- r "rv ruby"
+
+# paths
 abbr -a -- infopaths 'printf "%s\\n" $INFOPATH'
-abbr -a -- j jj
-abbr -a -- jsonfmt "prettier --stdin-filepath=o.json"
-abbr -a -- jwtd 'jc --jwt | jq'
-abbr -a -- k 'lsr --group-directories-first --long'
-abbr -a -- ka 'lsr --group-directories-first --long --almost-all'
-abbr -a -- kube-clear-context "kubectl config unset current-context"
-abbr -a -- ls 'lsr --hyperlinks=never --group-directories-first'
 abbr -a -- manpaths 'printf "%s\\n" $MANPATH'
 abbr -a -- paths 'printf "%s\\n" $PATH'
+
+# jujutsu vcs
+abbr -a -- j jj
+abbr -a -- ji jjui
+abbr -a -- ch "jj git push --change"
+abbr -a -- jd "jj desc"
+abbr -a -- jf 'jj git fetch'
+abbr -a -- jn "jj new"
+abbr -a -- js "jj status"
+
+# kubernetes
+abbr -a -- k kubectl
+abbr -a -- kd "k9s --readonly"
+abbr -a -- kn "kubectl ns"
+abbr -a -- kc "kubectl ctx"
+
+# clear env/contexts
+abbr -a -- clear_gcp "gcloud config unset project"
+abbr -a -- clear_aws 'set -e AWS_PROFILE'
+abbr -a -- clear_k8s "kubectl config unset current-context"
+
+# misc
+abbr -a -- bathelp 'bat --plain --language=help'
+abbr -a -- cato cato-sdp
+abbr -a -- cert 'tls_cert_info -address'
+abbr -a -- count_uniq 'sort | uniq -c | sort -nr'
+abbr -a -- gron fastgron
+abbr -a -- fmt_json "prettier --stdin-filepath=o.json"
+abbr -a -- jwtd 'jc --jwt | jq'
 abbr -a -- prune-symlinks 'find -L . -name . -o -type d -prune -o -type l -exec rm {} +'
 abbr -a -- tolower 'tr "[:upper:]" "[:lower:]"'
 abbr -a -- toupper 'tr "[:lower:]" "[:upper:]"'
 abbr -a -- ungron 'fastgron --ungron'
 abbr -a -- vimdiff difft
 abbr -a -- wv_kid_to_uuid "base64 --decode | xxd -p | python -c 'import sys,uuid; print(uuid.UUID(hex=sys.stdin.readline().rstrip()))'"
-abbr -a -- age-encrypt "age -e -a -r (cat ~/iCloud/Documents/age-key/public.txt)"
-abbr -a -- age-decrypt "age -d -i ~/iCloud/Documents/age-key/key.txt"
-abbr -a -- ku kubectl
-abbr -a -- h hx
-abbr -a -- hh "hx --health"
-abbr -a -- n nvim
-abbr -a -- ji jjui
-abbr -a -- k9s "k9s --readonly"
-abbr -a -- kd "k9s --readonly"
-abbr -a -- kc "kubectl config unset current-context"
-abbr -a -- kx "kubectl ctx"
-abbr -a -- ns "kubectl ns"
-abbr -a -- m mise
-abbr -a -- r "rv ruby"
-abbr -a -- js "jj status"
-abbr -a -- jd "jj desc"
-abbr -a -- jn "jj new"
-abbr -a -- ch "jj git push --change"
-abbr -a -- e zed
-abbr -a -- ac 'set -e AWS_PROFILE'
-abbr -a -- cato cato-sdp
