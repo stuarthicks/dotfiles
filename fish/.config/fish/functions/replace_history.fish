2f1a691f4fc1f9f2235d7e0e686a6994faf172a5
Stuart Hicks <stuarthicks@users.noreply.github.com>
2026-03-10 11:46:50 +0000

    replicate zsh hist replace in fish

---
+++ fish/.config/fish/functions/replace_history.fish
@@ -0,0 +1,6 @@
+abbr histreplace --regex '\^.*\^.*' --function replace_history --position anywhere
+
+function replace_history
+    set -l kv (string split '^' -- $argv[1])
+    string replace -- $kv[2] $kv[3] $history[1]
+end
