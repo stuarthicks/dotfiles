eeaf140c696369f01f9582e2b6b72787fd6aa47a
Stuart Hicks <stuarthicks@users.noreply.github.com>
2026-03-10 11:50:18 +0000

    fix hist replace

--- fish/.config/fish/conf.d/abbreviations.fish
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -52,3 +52,5 @@ abbr -a -- toupper 'tr "[:lower:]" "[:upper:]"'
 abbr -a -- ungron 'fastgron --ungron'
 abbr -a -- vimdiff difft
 abbr -a -- wv_kid_to_uuid "base64 --decode | xxd -p | python -c 'import sys,uuid; print(uuid.UUID(hex=sys.stdin.readline().rstrip()))'"
+
+abbr histreplace --regex '\^.*\^.*' --function replace_history --position anywhere
--- fish/.config/fish/functions/replace_history.fish
+++ fish/.config/fish/functions/replace_history.fish
@@ -1,5 +1,3 @@
-abbr histreplace --regex '\^.*\^.*' --function replace_history --position anywhere
-
 function replace_history
     set -l kv (string split '^' -- $argv[1])
     string replace -- $kv[2] $kv[3] $history[1]
