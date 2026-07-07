159d55ccbaa3cbb2790c1f79cffe2fb1eed4959e
Stuart Hicks <stuarthicks@users.noreply.github.com>
2026-07-07 21:59:22 +0100

    ghostel.el

--- emacs/.config/doom/custom.el
+++ emacs/.config/doom/custom.el
@@ -4,4 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor-type 'bar))
+ '(cursor-type 'bar)
+ '(package-selected-packages '(evil-ghostel ghostel helix)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
