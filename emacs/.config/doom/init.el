c9e901d9575936e7acfaad527edc2f57757a93d2
Stuart Hicks <stuarthicks@users.noreply.github.com>
2026-07-16 16:23:41 +0100

    fix typos

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -158,7 +158,7 @@
   nix               ; I hereby declare "nix geht mehr!"
   ;;ocaml             ; an objective camel
   ;;odin              ; C, minus its footguns
-  (org +pretty +dragndrop +jupyter +pancoc +pomodoro +present +roam2)               ; organize your plain life in plain text
+  (org +pretty +dragndrop +jupyter +pandoc +pomodoro +present +roam)               ; organize your plain life in plain text
   ;;php               ; perl's insecure younger brother
   ;;plantuml          ; diagrams for confusing people more
   ;;graphviz          ; diagrams for confusing yourself even more
